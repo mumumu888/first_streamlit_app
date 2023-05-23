7bc8918ed06469fc78a6dea7917e20966dc1bc6e
mumumu888 <134301853+mumumu888@users.noreply.github.com>
2023-05-23 11:40:48 +0900

    Update streamlit_app.py
    Snowflake badge2 lesson3-1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header('朝食メニュー')
+streamlit.text('オメガ 3 とブルーベリー オートミール')
+streamlit.text('ケール、ほうれん草、ロケットスムージー')
+streamlit.text('平飼い卵の固ゆで')
