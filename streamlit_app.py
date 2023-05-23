4f7fab1c595f63348fdf2d71a4fd4c646aeca09a
mumumu888 <134301853+mumumu888@users.noreply.github.com>
2023-05-23 23:12:04 +0900

    Update streamlit_app.py
    Snowflake Lesson9-1

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,3 +17,8 @@ Fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # ページにテーブルを表示します。
 streamlit.dataframe(Fruits_to_show)
+
+
+import requests
+fruityvice_response =requests.get("https://fruityvice.com/api/fruit/watermelon") 
+streamlit.text(fruityvice_response)
